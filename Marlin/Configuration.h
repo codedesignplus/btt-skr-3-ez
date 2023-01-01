69d08372b2863d0e05315719ee1a4064c6664e1e
CodeDesignPlus <codedesignplus@outlook.com>
2023-01-01 18:25:08 -0500

    feat: Set PID Hotend feat: Set PID Bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -667,9 +667,13 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    // #define DEFAULT_Kp  22.20
+    // #define DEFAULT_Ki   1.08
+    // #define DEFAULT_Kd 114.00
+
+    #define DEFAULT_Kp 21.3736
+    #define DEFAULT_Ki 1.5716
+    #define DEFAULT_Kd 72.6701
   #endif
 #endif
 
@@ -734,7 +738,7 @@
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  * @section bed temp
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -752,9 +756,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 170.6789
+  #define DEFAULT_bedKi 21.9947
+  #define DEFAULT_bedKd 882.9790
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
